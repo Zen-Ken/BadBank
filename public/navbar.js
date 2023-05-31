c6f44c5803a653986307ff69e7e553b0dc630113
Kenny Luong <83483633+Zen-Ken@users.noreply.github.com>
2023-05-31 19:28:39 -0400

    Add files via upload

---
+++ public/navbar.js
@@ -0,0 +1,44 @@
+function NavBar(){
+  const {user , setUser}        = React.useContext(UserContext);
+  
+  return(
+
+  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <div className="container-fluid">
+      <a className="navbar-brand" href="#">BadBank</a>
+      {JSON.stringify(user) === '{}' ?
+      <>
+      {/* <li className="nav-item">
+        <a className="nav-link" href="#/login/">Login</a>
+      </li> */}
+      </>
+      : 
+      <>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+          <li className="nav-item">
+            <a className="nav-link" href="#/login/">Account</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#/deposit/">Deposit</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#/withdraw/">Withdraw</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#/balance/">Balance</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#/alldata/">AllData</a>
+          </li>   
+        </ul>
+      </div>
+      </>}
+    </div>
+    </nav>
+
+  );
+}
