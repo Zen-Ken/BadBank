3d50b7f5920ff598c24fcabddb2f1ac198cd7083
Zen-Ken <kenluong456@gmail.com>
2023-08-15 18:05:46 -0400

    password reset updated

--- src/passwordreset.js
+++ src/passwordreset.js
@@ -2,7 +2,7 @@ import { React, useContext, useState, useEffect } from "react";
 import { UserContext, Card } from "./context";
 
 export function PasswordReset(){
-    const { user, setUser }     = useContext(UserContext);
+    const [email, setEmail]     = useState('');
     const [show, setShow]       = useState(true);
     const [status, setStatus]   = useState('');    
   
@@ -10,14 +10,42 @@ export function PasswordReset(){
     return (
       <Card
         bgcolor="secondary"
-        header={show ? "Password Reset" : "Account"}
+        header="Password Reset"
         status={status}
-        body={
-         <>
-            Email has been sent
-         </>}
+        body={show ?
+          <ResetEmailEntry email={email} setEmail={setEmail} setShow={setShow}/>:
+          <NewPassword email={email} setEmail={setEmail} setShow={setShow}/>
+        }
       />
     ) 
   }
 
-  
+  function ResetEmailEntry(props){
+    function handle(){
+      props.setShow(false)
+    }
+
+    return (<>
+      Email<br/>
+      <input type="input" 
+        className="form-control" 
+        placeholder="Enter email" 
+        value={props.email} 
+        onChange={e => props.setEmail(e.currentTarget.value)}/>
+      <button id="btnLogin" type="submit" className="btn btn-light ml-1 my-1" onClick={handle}>Send</button><br/>
+      </>
+    )
+  }
+
+  function NewPassword(props){
+    return (<>
+      New Password<br/>
+      <input type="input" 
+        className="form-control" 
+        placeholder="Enter New" 
+        value={props.email} 
+        onChange={e => props.setEmail(e.currentTarget.value)}/>
+      <button id="btnLogin" type="submit" className="btn btn-light ml-1 my-1">Send</button><br/>
+      </>
+    )
+  }
